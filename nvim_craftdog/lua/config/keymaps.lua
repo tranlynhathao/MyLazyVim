5bd77491cf008df29cf1cd03ab4c2edf43541496
tranlynhathao <haotran04022005@gmail.com>
2024-07-04 09:01:15 +0700

    upload new version of craftdog

---
+++ nvim_craftdog/lua/config/keymaps.lua
@@ -0,0 +1,83 @@
+local discipline = require("craftzdog.discipline")
+
+discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"2p')
+keymap.set("n", "<Leader>P", '"2P')
+keymap.set("v", "<Leader>p", '"2p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+keymap.set("n", "Y", "y$", { noremap = true, silent = true })
+keymap.set("n", "U", ":redo<cr>", { noremap = true, silent = true })
+-- keymap.set("n", "p", "mz`[V`]`zP", { noremap = true, silent = true })
+-- keymap.set("n", "P", "mz`[V`]`zp", { noremap = true, silent = true })
+keymap.set("n", "p", "o<esc>p", { noremap = true, silent = true })
+keymap.set("n", "P", "O<esc>p", { noremap = true, silent = true })
+
+-- Replace text
+keymap.set("n", "r", ":s//g<left><left>", { noremap = true, silent = true })
+keymap.set("n", "rr", ":%s//g<left><left>", { noremap = true, silent = true })
+keymap.set("v", "r", ":s//g<left><left>", { noremap = true, silent = true })
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+	vim.diagnostic.goto_next()
+end, opts)
+
+keymap.set("n", "<leader>r", function()
+	require("craftzdog.hsl").replaceHexWithHSL()
+end)
+
+keymap.set("n", "<leader>i", function()
+	require("craftzdog.lsp").toggleInlayHints()
+end)
---
+++ nvim_craftdog/lua/plugins/vim_visual_multi.lua
@@ -0,0 +1,33 @@
+-- ~/.config/nvim/lua/plugins/vim_visual_multi.lua
+
+return {
+	"mg979/vim-visual-multi",
+	branch = "master",
+	config = function()
+		vim.g.VM_default_mappings = 0
+		-- available keymap
+		vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>(VM-Find-Under)", {})
+		vim.api.nvim_set_keymap("x", "<C-n>", "<Plug>(VM-Find-Subword-Under)", {})
+		vim.api.nvim_set_keymap("n", "<C-Down>", "<Plug>(VM-Add-Cursor-Down)", {})
+		vim.api.nvim_set_keymap("n", "<C-Up>", "<Plug>(VM-Add-Cursor-Up)", {})
+
+		-- advance keymap
+		vim.api.nvim_set_keymap("n", "<C-S-Down>", "<Plug>(VM-Select-Next)", {})
+		vim.api.nvim_set_keymap("n", "<C-S-Up>", "<Plug>(VM-Select-Prev)", {})
+		vim.api.nvim_set_keymap("n", "<C-d>", "<Plug>(VM-Remove-Cursor)", {})
+		vim.api.nvim_set_keymap("n", "<C-S-d>", "<Plug>(VM-Remove-All)", {})
+		vim.api.nvim_set_keymap("n", "<C-l>", "<Plug>(VM-Reselect-Last)", {})
+		vim.api.nvim_set_keymap("n", "<C-a>", "<Plug>(VM-Select-All)", {})
+		vim.api.nvim_set_keymap("n", "<C-/>", "<Plug>(VM-Visual-Clear)", {})
+		vim.api.nvim_set_keymap("n", "<C-Space>", "<Plug>(VM-Visual-Regex)", {})
+
+		-- switch between cursors
+		vim.api.nvim_set_keymap("n", "n", "<Plug>(VM-Find-Under)", {})
+		vim.api.nvim_set_keymap("n", "N", "<Plug>(VM-Select-Next)", {})
+		vim.api.nvim_set_keymap("n", "p", "<Plug>(VM-Select-Prev)", {})
+
+		-- lock and unclock the current cursor
+		vim.api.nvim_set_keymap("n", "<C-s>", "<Plug>(VM-Insert)", {})
+		vim.api.nvim_set_keymap("n", "<C-r>", "<Plug>(VM-Replace)", {})
+	end,
+}
